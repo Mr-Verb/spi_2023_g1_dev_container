83bed8d66e194100a2937468f3a2fad2e4a87a6a
William James Moorcraft <william.moorcraft@student.adelaide.edu.au>
2023-03-17 09:39:27 +1030

    updated script and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,21 +5,14 @@ RUN npm install -g npm@latest
 # node-gyp@latest
 RUN apk add python3 make g++ git curl
 
-# Ensure default `node` user has access to `sudo`
-#ARG USERNAME=node
-#RUN echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME \
-#   && chmod 0440 /etc/sudoers.d/$USERNAME
-
 # Install base dependencies
-#RUN npm install -g npm@9.6.1
-#RUN npm install -g sfdx-cli@latest  make@latest gcc@latest graceful-fs@latest uuid@latest
 RUN yarn add electron-builder --dev
-#RUN npm install -g electron@latest
-#RUN npm update -g && npm outdated
-
 WORKDIR /usr/app
 RUN npm install -g nodehun@latest
 
+# Run yarn install after the container is launched
+RUN yarn cache clean
+CMD sleep 5 ; cd /home/Zettlr ; yarn install --frozen-lockfile ; sleep infinity
 
 # Set `DEVCONTAINER` environment variable to help with orientation
 ENV DEVCONTAINER=true
--- container_builder.sh
+++ container_builder.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 git --version
-echo "Before going further ensure you fork the Zettlr Repo into your GitHub"
-echo -n "Enter GitHub Username: "
+echo "Before going further ensure you fork the Zettlr Repo into your GitHub and it has been cloned into the current directory. Press any key to continue:"
+#echo -n "Enter GitHub Username: "
 read USERNAME
 NEEDED_PATH=""
 
 case $OSTYPE in
     msys*)
         echo "MSYS dectected!"
-        NEEDED_PATH=/$PWD
-        git clone https://github.com/$USERNAME/Zettlr.git
+        NEEDED_PATH=/$PWD/Zettlr
+        #git clone https://github.com/$USERNAME/Zettlr.git
         echo $NEEDED_PATH
         ;;
     darwin*)
@@ -20,6 +20,7 @@ case $OSTYPE in
         ;;
 esac
 
-docker build -t spi_2023_g1_dev_container_image:1.1.13 .
-docker run -d -it --name dev_container --mount type=bind,source="$NEEDED_PATH",target=/home/Zettlr spi_2023_g1_dev_container_image:1.1.13
+docker build -t spi_2023_g1_dev_container_image:1.2.0 .
+docker run -d -it --name SPI_G1_Dev_Container --mount type=bind,source="$NEEDED_PATH",target=/home/Zettlr spi_2023_g1_dev_container_image:1.2.0
 echo "Now click the the green connection button in the bottom right corner and select 'attach to running container'"
+echo -n "Once conntected navigate to /home/Zettlr and 'yarn install --frozen-lockfile'"
