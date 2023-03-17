de26d87cf5ae94369d4687f9a484e4fbe8260508
Will <william.moorcraft12@gmail.com>
2023-03-17 15:33:30 +1030

    Updated container

--- container_builder.sh
+++ container_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git --version
-echo "Before going further ensure you fork the Zettlr Repo into your GitHub and it has been cloned into the current directory. Press any key to continue:"
+#git --version
+echo "Before going further ensure you fork the Zettlr Repo into your GitHub and the Zettlr folder cloned into the current directory. Press Enter to continue:"
 #echo -n "Enter GitHub Username: "
 read USERNAME
 NEEDED_PATH=""
@@ -9,7 +9,6 @@ case $OSTYPE in
     msys*)
         echo "MSYS dectected!"
         NEEDED_PATH=/$PWD/Zettlr
-        #git clone https://github.com/$USERNAME/Zettlr.git
         echo $NEEDED_PATH
         ;;
     darwin*)
@@ -24,7 +23,7 @@ case $OSTYPE in
         ;;
 esac
 
-docker build -t spi_2023_g1_dev_container_image:1.2.0 .
-docker run -d -it --name SPI_G1_Dev_Container --mount type=bind,source="$NEEDED_PATH",target=/home/Zettlr spi_2023_g1_dev_container_image:1.2.0
+#docker build -t spi_2023_g1_dev_container_image:1.2.0 .
+docker run -d -it --name SPI_G1_Dev_Container --mount type=bind,source="$NEEDED_PATH",target=/home/Zettlr mrverb/spi_2023_g1_dev_container_image:1.2.0
 echo "Now click the the green connection button in the bottom right corner and select 'attach to running container'"
-echo -n "Once conntected navigate to /home/Zettlr and 'yarn install --frozen-lockfile'"
+echo -n "Once conntected navigate to /home/Zettlr"
